6581f7c6faf8daaecdf8f84df8393c3444db0afc
Julian Andres Pereira <56051618+Nzone56@users.noreply.github.com>
2026-04-16 13:37:11 -0500

    Revert "feat #1 - Initial web repository setup"

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,6 @@ const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
   // Override default ignores of eslint-config-next.
-  {
-    rules: {
-      "@next/next/no-img-element": "off",
-    },
-  },
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
